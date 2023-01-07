6a4691c982354f4581d9a20a75f4a4470f23fa52
Pavel <cemenru@mail.ru>
2023-01-07 22:52:07 +0300

    reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+  let array = ('' + n).split('');
+  let result = '';
+
+  if (array[0] == '-') {
+    for (let i = 1; i < array.length; i++) {
+      result = array[i] + result
+    }
+    return +result;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      result = array[i] + result;
+    }
+    return +result;
+  }
+
 }
